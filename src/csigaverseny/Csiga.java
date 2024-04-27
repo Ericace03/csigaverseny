cc18a4c1451d9ebc2c08872d5b027cd766cfa8d1
Ericace03 <ericacebmf@gmail.com>
2024-04-27 21:51:35 +0200

    added korok, lepesek

--- src/csigaverseny/Csiga.java
+++ src/csigaverseny/Csiga.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package csigaverseny;
+import java.util.Random;
 
 /**
  *
@@ -12,21 +13,26 @@ public class Csiga {
     
     private int messze;
     private boolean kap;
+    private String szin;
     private int sebesseg;
     
     public static final String CSIGA_PIROS = "\033[31m";
     public static final String CSIGA_ZOLD = "\033[32m";
     public static final String CSIGA_KEK = "\033[34m";
 
-    public Csiga() {
+    public Csiga(String szin) {
   
-        this.messze = 0;
         this.kap = false;
+        this.szin = szin;
         this.sebesseg = 0;
     }
 
-    public int isMessze() {
+    public int getMessze() {
         return messze;
+    }
+    
+    public void setMessze(int messze) {
+        this.messze = messze;
     }
 
     public void setKap(int messze) {
@@ -40,6 +46,14 @@ public class Csiga {
     public void setKap(boolean kap) {
         this.kap = kap;
     }
+    
+    public String getSzin() {
+        return szin;
+    }
+
+    public void setSzin(String szin) {
+        this.szin = szin;
+    }
 
     public int getSebesseg() {
         return sebesseg;
@@ -47,5 +61,10 @@ public class Csiga {
 
     public void setSebesseg(int sebesseg) {
         this.sebesseg = sebesseg;
+    }
+    
+    public void randomSebesseg() {
+        Random random = new Random();
+        this.sebesseg = random.nextInt(4);
     }
 }
--- src/csigaverseny/Jatek.java
+++ src/csigaverseny/Jatek.java
@@ -4,7 +4,6 @@
  */
 package csigaverseny;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,7 +18,6 @@ public class Jatek {
     private int fogad;
     private double esely;
     private boolean csigagyorsito;
-    private static Random rnd = new Random();
     
     public Jatek() {      
         start();
@@ -76,12 +74,13 @@ public class Jatek {
     public void start() {
        
         Scanner scanner = new Scanner(System.in);
+        Csiga[] csigak = new Csiga[3];
+        csigak[0] = new Csiga(Csiga.CSIGA_PIROS);
+        csigak[1] = new Csiga(Csiga.CSIGA_ZOLD);
+        csigak[2] = new Csiga(Csiga.CSIGA_KEK);
         //Jatek jatek = new Jatek();
         
-        System.out.println("Start");
-        System.out.println(Csiga.CSIGA_PIROS+"@");
-        System.out.println(Csiga.CSIGA_ZOLD+"@");
-        System.out.println(Csiga.CSIGA_KEK+"@");
+        System.out.println("Csigaverseny: "+Csiga.CSIGA_PIROS+"@ "+Csiga.CSIGA_ZOLD+"@ "+Csiga.CSIGA_KEK+"@");
         System.out.println("Melyik csiga fog nyerni? 1-Piros, 2-Zöld, 3-Kék. Tippelj: ");
         
         int fogad = scanner.nextInt();
@@ -98,9 +97,35 @@ public class Jatek {
             case 3:
                 fogadas = Csiga.CSIGA_KEK+"A Kék @-ra fogadtál!";
                 break;
+            }
+        
+        System.out.println(fogadas);
+            
+        System.out.println("Start");
+        
+        for (Csiga csiga : csigak) {
+            System.out.println(csiga.getSzin() + "@");
         }
-            System.out.println(fogadas);
-    }
+                    
+            for (int kor = 0; kor < 5; kor++) {
+                
+                System.out.println((kor+1)+". kör");
+                
+                    for (Csiga csiga : csigak) {
+                        csiga.randomSebesseg();
+                        csiga.setMessze(csiga.getMessze() + csiga.getSebesseg()); 
+                
+                        StringBuilder lepesPont = new StringBuilder();
+                        for (int j = 0; j < csiga.getMessze(); j++) {
+                        lepesPont.append(".");
+                        }
+                        
+                        System.out.println(csiga.getSzin() + lepesPont.toString() + "@");
+                    //System.out.println("\033[31m"+csigak[0].getMessze()+ "@");
+                    //System.out.println("\033[31m"+stepsDot.toString() + "@");
+                    }
+               
+            }
+            }
 }
     
-
